a754bff1758b86d55544d0b1bb8fe2b8dbc57028
Javier Reyes Herrera <J4vierR3yes11@gmail.com>
2026-05-05 12:28:40 +0000

    Creating Webapp

---
+++ AutoStar/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoStar.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
